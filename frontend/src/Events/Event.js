4ccfa105f33f1ecaf8b2a4ce36250335b30ab4ef
Cameron Faith <43867542+cmrnfaith@users.noreply.github.com>
2022-03-26 14:16:21 -0600

    Event items designed

--- frontend/src/Events/Event.js
+++ frontend/src/Events/Event.js
@@ -1,5 +1,25 @@
 import React, { useState } from "react";
 
-const CreateEvent = () => {
-  return <div className="event-container"></div>;
+const Event = ({ event }) => {
+  return (
+    <div className="event-container">
+      <div className="title">{event.name}</div>
+      <div className="date">{new Date(event.date).toLocaleDateString()}</div>
+      <div className="type">{event.eventType}</div>
+      <div className="faculty">{event.faculty}</div>
+      <div className="location">{event.location}</div>
+      <div className="private">
+        {event.private === 0 ? "Public" : "Private"}
+      </div>
+      {/* <div className="time">{new Date(event.date).toLocaleTimeString()}</div> */}
+      {/* <div className="duration">{event.duration}</div> */}
+      {/* <div className="text">{event.description}</div> */}
+      {/* <div className="attire">{event.attire}</div> */}
+      {/* <div className="membership">{event.membership}</div> */}
+      {/* <div className="price">
+        {event.price === 0 ? "Free" : "$" + event.price.toFixed(2)}
+      </div> */}
+    </div>
+  );
 };
+export default Event;
