2a73a62a2570f0b5ca25df41757b4d03773722c0
Dylan Rae <55325297+djohnjames@users.noreply.github.com>
2022-03-26 12:32:05 -0600

    Started event creation endpoint

--- backend/databaseFunctions.py
+++ backend/databaseFunctions.py
@@ -17,3 +17,13 @@ def get_all_events(conn:mysql.connector.connect, table_name:str)->List:
     cur.close()
 
     return results
+
+def insert_event_db(conn:mysql.connector.connect, table_name:str)->List:
+    """Gets the user that matches the username passed in.
+    Args:
+        conn (mysql.connector.connection): A valid connection to the mySQL Database
+        table_name (str): Table name in DB to check
+    Returns:
+        list: Every event in the database
+    """
+    pass
