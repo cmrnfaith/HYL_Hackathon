3e805536531eabdfd12c3b4e1583724c6539138f
Aidan Johnson <aidan.johnson1@ucalgary.ca>
2022-03-26 11:11:09 -0600

    Added some planned apis on the backend.

--- backend/api.py
+++ backend/api.py
@@ -7,3 +7,30 @@ from flask_login.utils import logout_user
 from main import login_manager
 from flask import request, send_from_directory, session
 from flask_login import current_user, login_user, login_required
+
+
+
+
+# ========================================================
+# Event APIs
+# ========================================================
+
+
+# Create event
+# Update event
+# Delete event
+# Get event
+# Get all events
+
+
+# search for specific event (will be based on a query)
+
+
+# ========================================================
+# User APIs
+# ========================================================
+
+# create user
+# login
+# logout
+# update user information
