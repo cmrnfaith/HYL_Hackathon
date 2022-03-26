96e5dd7c315439b409041a3a31dd3549ed9afc6f
Cameron Faith <43867542+cmrnfaith@users.noreply.github.com>
2022-03-26 11:52:23 -0600

    Login/ logout added

---
+++ frontend/src/Logout/Logout.js
@@ -0,0 +1,52 @@
+//=============React Elements=============//
+import React, { useState, useEffect } from "react";
+import { Redirect } from "react-router-dom";
+
+const Logout = ({ loginStatus, updateLoginStatus, updateLocalUser }) => {
+  const [redirect, setRedirect] = useState(false);
+  const [redirectUrl, setRedirectUrl] = useState("/");
+  useEffect(() => {
+    if (loginStatus) {
+      updateLoginStatus(false);
+      updateLocalUser({
+        username: "",
+        password: "",
+        country: "",
+        is_admin: "",
+        date_of_birth: "",
+        first_name: "",
+        last_name: "",
+        email: "",
+      });
+
+      fetch("/api/logout/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        credentials: "include",
+        body: JSON.stringify({}),
+      })
+        .then(async (res) => {
+          if (res.status === 200) {
+            setRedirect(true);
+          } else {
+            setRedirectUrl("/viewCrypto");
+            setRedirect(true);
+          }
+        })
+        .catch((error) => {
+          // Handle error
+        });
+    }
+  }, [loginStatus, updateLoginStatus, updateLocalUser]);
+
+  return (
+    <div>
+      {redirect ? <Redirect push to={redirectUrl} /> : null}Logout Unsuccessful
+    </div>
+  );
+};
+
+export default Logout;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,5 @@
 //=============React Elements=============//
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
@@ -7,16 +7,57 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 //=============Components=============//
 import Homepage from "./Home/Homepage";
 import ScrollToTop from "./utilities/ScrollToTop";
-// import PrivateRoute from "./OtherComponents/PrivateRoute";
+import PrivateRoute from "./OtherComponents/PrivateRoute";
 import Events from "./Events/Events";
 import Example from "./Example/Example";
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import NotFound from "./NotFound/NotFound";
+import Login from "./Login/Login";
+import Logout from "./Logout/Logout";
+import SignUp from "./SignUp/SignUp";
 
-const loginStatus = true;
+export const defaultUser = [
+  {
+    username: "",
+    password: "",
+    country: "",
+    is_admin: "",
+    date_of_birth: "",
+    first_name: "",
+    last_name: "",
+    email: "",
+  },
+];
 
 const App = () => {
+  const [loginStatus, setLoginStatus] = useState(
+    localStorage.getItem("loginStatus") === "true"
+  );
+
+  const [user, setUser] = useState(
+    JSON.parse(localStorage.getItem("user")) !== null
+      ? JSON.parse(localStorage.getItem("user"))
+      : defaultUser
+  );
+
+  const updateLoginStatus = (value) => {
+    localStorage.setItem("loginStatus", JSON.stringify(value));
+    setLoginStatus(value);
+    console.log(localStorage.getItem("loginStatus"));
+  };
+
+  const updateLocalUser = (newUser) => {
+    localStorage.setItem("user", JSON.stringify(newUser));
+    console.log(localStorage.getItem("user"));
+    setUser(newUser);
+  };
+
+  function withProps(Component, props) {
+    return function (matchProps) {
+      return <Component {...props} {...matchProps} />;
+    };
+  }
   return (
     <BrowserRouter>
       <Header className="header" loginStatus={loginStatus} />
@@ -25,11 +66,23 @@ const App = () => {
 
       <Switch className="content">
         {/* Normal Navagation Routes */}
+
         <Route exact path="/" component={Homepage} />
         <Route exact path="/events" component={Events} />
         <Route exact path="/example" component={Example} />
         <Route exact path="/example2" component={Example} />
+        <Route path="/signup" component={SignUp} />
 
+        <PrivateRoute
+          redirect_url="/login"
+          loginStatus={loginStatus}
+          path="/logout"
+          component={withProps(Logout, {
+            loginStatus,
+            updateLoginStatus,
+            updateLocalUser,
+          })}
+        />
         <Route path="*" exact={true} component={NotFound} />
       </Switch>
       <Footer className="footer" />
