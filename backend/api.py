d3d655d5a52557fb6507debb9521a6bc0820dc19
Aidan Johnson <aidan.johnson1@ucalgary.ca>
2022-03-26 12:19:57 -0600

    Got the backend up and running.

--- backend/api.py
+++ backend/api.py
@@ -4,7 +4,7 @@ from datetime import datetime as dt
 import numpy as np
 from flask.wrappers import Response
 from flask_login.utils import logout_user
-from main import login_manager
+from main import app, login_manager
 from flask import request, send_from_directory, session
 from flask_login import current_user, login_user, login_required
 
