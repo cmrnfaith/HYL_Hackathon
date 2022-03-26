8ad625bf5e11b7858f9a78f56b10ec84d60632a5
Michele Piperni <michele.piperni1@gmail.com>
2022-03-26 17:51:28 -0600

    create event is done

--- frontend/src/CreateEvent/CreateEvent.js
+++ frontend/src/CreateEvent/CreateEvent.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const CreateEvent = () => {
   const [eventName, setEventName] = useState("");
@@ -8,23 +8,77 @@ const CreateEvent = () => {
   const [eventDuration, setEventDuration] = useState("");
   const [eventLocation, setEventLocation] = useState("");
   const [eventType, setEventType] = useState("");
-  const [eventFaculty, setEventFaculty] = useState("");
+
   const [eventPrice, setEventPrice] = useState("");
+  const [eventPriceChecked, setEventPriceChecked] = useState(false);
+
+  const [eventFaculty, setEventFaculty] = useState("");
+  const [eventFacultyChecked, setEventFacultyChecked] = useState(false);
+
+  const [eventAttire, setEventAttire] = useState("");
+  const [eventAttireChecked, setEventAttireChecked] = useState(false);
+
+  const [eventMembershipRequirement, setEventMembershipRequirement] =
+    useState("");
+  const [
+    eventMembershipRequirementChecked,
+    setEventMembershipRequirementChecked,
+  ] = useState(false);
+
+  const [eventPublic, setEventPublic] = useState("");
+  const [eventPublicChecked, setEventPublicChecked] = useState(false);
 
   const [today, setToday] = useState("");
+
+  const handleEventPriceChecked = () => {
+    setEventPriceChecked(!eventPriceChecked);
+  };
+
+  const handleEventFacultyChecked = () => {
+    setEventFacultyChecked(!eventFacultyChecked);
+  };
+
+  const handleEventAttireChecked = () => {
+    setEventAttireChecked(!eventAttireChecked);
+  };
+
+  const handleEventMembershipRequirementChecked = () => {
+    setEventMembershipRequirementChecked(!eventMembershipRequirementChecked);
+  };
+
+  const handleEventPublicChecked = () => {
+    setEventPublicChecked(!eventPublicChecked);
+  };
+
+  const getToday = () => {
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth() + 1; //January is 0!
+    var yyyy = today.getFullYear();
+
+    if (dd < 10) {
+      dd = "0" + dd;
+    }
+
+    if (mm < 10) {
+      mm = "0" + mm;
+    }
+
+    today = yyyy + "-" + mm + "-" + dd;
+    setToday(today);
+  };
+
+  useEffect(getToday, [today]);
 
   const onSubmit = (e) => {
     e.preventDefault();
 
     submitEvent();
-    // setEventName("");
-    // setEventDescription("");
-    // setEventDate("");
-    // setEventTime("");
-    // setEventLocation("");
   };
 
   const submitEvent = () => {
+    var dateTimeTogether = eventDate + " " + eventTime + ":00";
+
     console.log({
       eventName,
       eventDescription,
@@ -33,8 +87,12 @@ const CreateEvent = () => {
       eventDuration,
       eventLocation,
       eventType,
-      eventFaculty,
       eventPrice,
+      eventFaculty,
+      eventAttire,
+      eventMembershipRequirement,
+      eventPublic,
+      dateTimeTogether,
     });
 
     var url = "/events";
@@ -48,32 +106,18 @@ const CreateEvent = () => {
       credentials: "include",
       body: JSON.stringify({
         name: eventName,
-        dateTime: "2022-08-01 16:20:00",
+        dateTime: dateTimeTogether,
         location: eventLocation,
         price: eventPrice,
-        attire: "casual",
-        membership: "0",
+        attire: null,
+        membership: eventMembershipRequirement,
         duration: eventDuration,
-        private: "0",
+        private: eventPublic,
         faculty: eventFaculty,
         description: eventDescription,
         eventType: eventType,
         hostID: "1",
       }),
-      // body: JSON.stringify({
-      //   name: "BSD TEST",
-      //   date: "2022-08-01 16:20:00",
-      //   location: "Sawatszy Household",
-      //   price: "10",
-      //   attire: "casual",
-      //   membership: "0",
-      //   duration: "2",
-      //   private: "0",
-      //   faculty: "Engineering",
-      //   description: "A great time for some boys",
-      //   eventType: "Party",
-      //   hostID: "1",
-      // }),
     })
       .then(async (res) => {
         if (res.status === 200) {
@@ -140,6 +184,7 @@ const CreateEvent = () => {
             <input
               value={eventDuration}
               min="0"
+              step="0.5"
               type="number"
               onChange={(e) => setEventDuration(e.target.value)}
               required
@@ -164,59 +209,184 @@ const CreateEvent = () => {
               onChange={(e) => setEventType(e.target.value)}
               required
             >
-              <option value="Sports">Sporting - Dinos</option>
-              <option value="University">University</option>
-              <option value="Club">Club</option>
+              <option value="">Choose an Event Type</option>
+              <option value="Sports">Sporting - Dinos Mens Volleyball</option>
+              <option value="Sports">Sporting - Dinos Womens Volleyball</option>
+              <option value="Sports">Sporting - Dinos Football</option>
+              <option value="Sports">Sporting - Dinos Mens Soccer</option>
+              <option value="Sports">Sporting - Dinos Womens Soccer</option>
+              <option value="Sports">Sporting - Dinos Mens Hockey</option>
+              <option value="Sports">Sporting - Dinos Womens Hockey</option>
+              <option value="Sports">
+                Sporting - Dinos Womens Field Hockey
+              </option>
+              <option value="Sports">Sporting - Dinos Swimming</option>
+              <option value="Sports">Sporting - Dinos Golf</option>
+              <option value="Sports">Sporting - Dinos Mens Basketball</option>
+              <option value="Sports">Sporting - Dinos Womens Basketball</option>
+              <option value="University">University - Student Union</option>
+              <option value="University">University - Student Union</option>
+              <option value="University">University - Student Union</option>
+              <option value="University">University - Student Union</option>
+              <option value="University">University - Student Union</option>
+              <option value="University">University - Student Union</option>
+              <option value="University">University - Student Union</option>
+              <option value="Club">Club - Digitronics</option>
+              <option value="Club">Club - Digitronics</option>
+              <option value="Club">Club - Digitronics</option>
+              <option value="Club">Club - Digitronics</option>
+              <option value="Club">Club - Digitronics</option>
+              <option value="Club">Club - Digitronics</option>
+              <option value="Club">Club - Digitronics</option>
+              <option value="Club">Club - Digitronics</option>
+              <option value="Club">Club - Digitronics</option>
+              <option value="Club">Club - Digitronics</option>
               <option value="Third Party">Third Party</option>
               <option value="Other">Other</option>
             </select>
           </div>
 
-          <div className="input-box">
-            <span className="input-label">Faculty</span>
-            <select
-              value={eventFaculty}
-              onChange={(e) => setEventFaculty(e.target.value)}
-              required
-            >
-              <option value="Engineering">Engineering</option>
-              <option value="Business">Business</option>
-              <option value="Arts">Arts</option>
-              <option value="Science">Science</option>
-              <option value="Education">Education</option>
-            </select>
-          </div>
-
-          <div className="input-box">
-            <span className="input-label">Price ($)</span>
-            <input
-              value={eventPrice}
-              placeholder="Event Price"
-              type="number"
-              onChange={(e) => setEventPrice(e.target.value)}
-              required
-            />
-          </div>
-
-          {/* <div className="input-box">
-            <span className="input-label">Add Tags</span>
-
-            <div className="input-checkbox-container">
+          {eventPriceChecked ? (
+            <div className="input-box">
+              <span className="input-label">Price ($)</span>
               <input
-                value={eventLocation}
-                className="input-checkbox"
-                type="checkbox"
-                onChange={(e) => setEventLocation(e.target.value)}
+                value={eventPrice}
+                placeholder="Event Price"
+                type="number"
+                onChange={(e) => setEventPrice(e.target.value)}
                 required
               />
-              <span className="input-checkbox-label">Sports</span>
             </div>
-          </div> */}
+          ) : (
+            <div></div>
+          )}
+
+          {eventFacultyChecked ? (
+            <div className="input-box">
+              <span className="input-label">Faculty</span>
+              <select
+                value={eventFaculty}
+                onChange={(e) => setEventFaculty(e.target.value)}
+                required
+              >
+                <option value="">Choose a Faculty</option>
+                <option value="Engineering">Engineering</option>
+                <option value="Business">Business</option>
+                <option value="Arts">Arts</option>
+                <option value="Science">Science</option>
+                <option value="Education">Education</option>
+              </select>
+            </div>
+          ) : (
+            <div></div>
+          )}
+
+          {eventAttireChecked ? (
+            <div className="input-box">
+              <span className="input-label">Event Attire</span>
+              <select
+                value={eventAttire}
+                onChange={(e) => setEventAttire(e.target.value)}
+                required
+              >
+                <option value="">Choose an Attire</option>
+                <option value="Formal">Formal</option>
+                <option value="Business Formal">Business Formal</option>
+                <option value="Costume">Costume</option>
+                <option value="Other">Other</option>
+              </select>
+            </div>
+          ) : (
+            <div></div>
+          )}
+
+          {eventMembershipRequirementChecked ? (
+            <div className="input-box">
+              <span className="input-label">Membership Requirement</span>
+              <input
+                value={eventMembershipRequirement}
+                type="text"
+                placeholder="(e.g. ESS Membership)"
+                onChange={(e) => setEventMembershipRequirement(e.target.value)}
+                required
+              />
+            </div>
+          ) : (
+            <div></div>
+          )}
+
+          {eventPublicChecked ? (
+            <div className="input-box">
+              <span className="input-label">Event Public/Private</span>
+              <select
+                value={eventPublic}
+                onChange={(e) => setEventPublic(e.target.value)}
+                required
+              >
+                <option value="">Choose either Public or Private</option>
+                <option value="Public">Public</option>
+                <option value="Private">Private</option>
+              </select>
+            </div>
+          ) : (
+            <div></div>
+          )}
 
           <div className="button">
             <input type="submit" value="Create Event" />
           </div>
         </form>
+
+        <div className="create-tags">
+          <h1 className="title">Add Tags</h1>
+
+          <div className="input-box">
+            <input
+              type="checkbox"
+              checked={eventPriceChecked}
+              onChange={handleEventPriceChecked}
+            />
+            <span className="input-label">Add a Price</span>
+          </div>
+
+          <div className="input-box">
+            <input
+              type="checkbox"
+              checked={eventFacultyChecked}
+              onChange={handleEventFacultyChecked}
+            />
+            <span className="input-label">Add a Faculty</span>
+          </div>
+
+          <div className="input-box">
+            <input
+              type="checkbox"
+              checked={eventAttireChecked}
+              onChange={handleEventAttireChecked}
+            />
+            <span className="input-label">Add an Attire</span>
+          </div>
+
+          <div className="input-box">
+            <input
+              type="checkbox"
+              checked={eventMembershipRequirementChecked}
+              onChange={handleEventMembershipRequirementChecked}
+            />
+            <span className="input-label">Add a Membership Requirement</span>
+          </div>
+
+          <div className="input-box">
+            <input
+              type="checkbox"
+              checked={eventPublicChecked}
+              onChange={handleEventPublicChecked}
+            />
+            <span className="input-label">
+              Make the Event Public or Private
+            </span>
+          </div>
+        </div>
       </div>
     </div>
   );
