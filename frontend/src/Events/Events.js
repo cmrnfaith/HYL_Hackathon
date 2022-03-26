36448022ed80dd9c0e5287dea4a83aa88b3d1d4b
Cameron Faith <43867542+cmrnfaith@users.noreply.github.com>
2022-03-26 12:37:10 -0600

    Events created

--- frontend/src/Events/Events.js
+++ frontend/src/Events/Events.js
@@ -1,7 +1,29 @@
 import { useEffect } from "react";
 
 const Events = () => {
-  useEffect(() => {}, []);
+  useEffect(() => {
+    var url = "/events";
+    fetch(url, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "applcation/json",
+      },
+      credentials: "include",
+    })
+      .then(async (res) => {
+        if (res.status === 200) {
+          var result = await res.json();
+          console.log(result);
+        } else if (res.status === 401) {
+        } else {
+          console.log("error fetching events");
+        }
+      })
+      .catch((error) => {
+        // Handle error
+      });
+  }, []);
   return (
     <div className="events-page-container">
       <h1>Events Page</h1>
