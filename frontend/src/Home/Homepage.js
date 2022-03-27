4b62b79479dbdae3a1d9dcf6985eeeb0681f6049
Michele Piperni <michele.piperni1@gmail.com>
2022-03-26 18:21:26 -0600

    homepage done

--- frontend/src/Home/Homepage.js
+++ frontend/src/Home/Homepage.js
@@ -1,7 +1,10 @@
+import photo1 from "../images/homepage_hub.jpg";
+
 const Homepage = () => {
   return (
     <div className="example-page-container">
-      <h1>HomePage</h1>
+      {/* <h1>HomePage</h1> */}
+      <img src={photo1} alt="A picture of the University" />
     </div>
   );
 };
